dbd0a5f8dd222f1c1a4dbf6389e9cc45d61f4f98
thongVuV <149690230+thongVuV@users.noreply.github.com>
2025-10-12 10:59:59 +0700

    13

--- assets/js/script.js
+++
@@ -1,5 +0,0 @@
-        const btn = document.getElementById('chatbot-btn');
-        const box = document.getElementById('chatbox');
-        btn.onclick = () => {
-            box.style.display = box.style.display === 'none' ? 'block' : 'none';
-        };
